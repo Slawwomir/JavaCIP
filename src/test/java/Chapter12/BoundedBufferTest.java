25f37b2c26abf4817737d8fa9b41a494057b1a2d
Sławomir Zaraziński <jestemKoderem@gmail.com>
2020-01-16 21:18:34 +0100

    Test take() when empty buffer.

--- src/test/java/Chapter12/BoundedBufferTest.java
+++ src/test/java/Chapter12/BoundedBufferTest.java
@@ -2,12 +2,11 @@ package Chapter12;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class BoundedBufferTest {
 
+    private static final long LOCKUP_DETECT_TIMEOUT = 5000;
     @Test
     public void testIsEmptyWhenConstructed() {
         BoundedBuffer<Integer> boundedBuffer = new BoundedBuffer<Integer>(10);
@@ -21,5 +20,27 @@ class BoundedBufferTest {
             boundedBuffer.put(i);
         }
         assertTrue(boundedBuffer.isFull());
+    }
+
+    @Test
+    public void testTakeBlocksWhenEmpty() {
+        final BoundedBuffer<Integer> bb = new BoundedBuffer<>(10);
+        Thread taker = new Thread(() -> {
+            try {
+                int unused = bb.take();
+                fail();
+            } catch (InterruptedException ignored) {
+            }
+        });
+
+        try {
+            taker.start();
+            Thread.sleep(LOCKUP_DETECT_TIMEOUT);
+            taker.interrupt();
+            taker.join(LOCKUP_DETECT_TIMEOUT);
+            assertFalse(taker.isAlive());
+        } catch (InterruptedException e) {
+            fail();
+        }
     }
 }
